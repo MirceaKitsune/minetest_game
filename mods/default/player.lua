274d4f16f5bd4bf6c763966effbdb7b698327969
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2012-11-12 17:51:01 +0200

    Update function names, plus add a test case for both player to player and lua-entity to player attachments

--- mods/default/player.lua
+++ mods/default/player.lua
@@ -15,8 +15,8 @@ function switch_player_visual()
 	
 	for _, obj in pairs(minetest.get_connected_players()) do
 		obj:set_properties(prop)
-		obj:set_animations({x=1, y=50}, 35, 0)
-		--obj:set_bone_posrot("", {x=0,y=0,z=0}, {x=0,y=0,z=0})
+		obj:set_animation({x=1, y=50}, 35, 0)
+		--obj:set_bone_position("", {x=0,y=0,z=0}, {x=0,y=0,z=0})
 	end
 
 	minetest.after(1.0, switch_player_visual)
@@ -25,7 +25,7 @@ minetest.after(1.0, switch_player_visual)
 
 -- Test case for attachments: An object is spawned and attached to the player with the specified name (use your own playername there) 10 seconds after the server starts
 
-test2 = {
+--[[test2 = {
   collisionbox = { 0, 0, 0, 0, 0, 0 },
   visual = "cube"
 }
@@ -33,7 +33,7 @@ test2 = {
 minetest.register_entity("default:test2", test2)
 
 function detachments(newobject)
-	newobject:set_detachment()
+	newobject:set_detach()
 	print ("Detached test object")
 end
 
@@ -49,12 +49,12 @@ function attachments()
 	local pos={x=0,y=0,z=0}
 	local newobject=minetest.env:add_entity(pos, "default:test2")
 	newobject:set_properties(prop)
-	newobject:set_animations({x=1, y=50}, 35, 0)
+	newobject:set_animation({x=1, y=50}, 35, 0)
 	print ("Spawned test object")
 
 	for _, obj in pairs(minetest.get_connected_players()) do
-		if(obj:get_player_name() == "some_nick") then
-			newobject:set_attachment(obj, "Bone", {x=0,y=0,z=0}, {x=0,y=0,z=0})
+		if(obj:get_player_name() == "MirceaKitsune") then
+			newobject:set_attach(obj, "Bone.001", {x=0,y=3,z=0}, {x=0,y=45,z=0})
 			print ("Attached test object to "..obj:get_player_name())
 		end
 	end
@@ -62,7 +62,24 @@ function attachments()
 	minetest.after(5.0, function() detachments(newobject) end)
 end
 
-minetest.after(15.0, attachments)
+minetest.after(15.0, attachments)]]
+
+-- Test case for player to player attachments
+
+function attachments_player()
+	for _, obj in pairs(minetest.get_connected_players()) do
+		if(obj:get_player_name() == "MirceaKitsune") then
+			for _, obj2 in pairs(minetest.get_connected_players()) do
+				if(obj2:get_player_name() == "MirceaKitsune1") then
+					obj2:set_attach(obj, "Bone.001", {x=0,y=6,z=0}, {x=0,y=45,z=0})
+					print ("Attached player "..obj2:get_player_name().." to player "..obj:get_player_name())
+				end
+			end
+		end
+	end
+end
+
+minetest.after(10.0, attachments_player)
 
 -- Definitions made by this mod that other mods can use too
 default = {}
