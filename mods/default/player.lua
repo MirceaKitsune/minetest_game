9b9b04ad1a52a7440130d2b2c908bf055350b399
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2012-11-14 23:48:19 +0200

    Punch / mine animation, and some consistency improvements to the player script

--- mods/default/player.lua
+++ mods/default/player.lua
@@ -3,8 +3,13 @@
 
 -- The API documentation in here was moved into doc/lua_api.txt
 
+-- Default animation speed. Special animations (such as the walk animation) should be offset from this factor
+animation_speed = 30
+
+-- Animation blending / transitioning amount
+animation_blend = 0
+
 -- Animations frame ranges:
--- Note: Walk animations should play at 30 speed for the default movement velocity
 animation_stand_START = 0
 animation_stand_END = 79
 animation_walk_forward_START = 81
@@ -15,6 +20,8 @@ animation_walk_right_START = 123
 animation_walk_right_END = 142
 animation_walk_left_START = 144
 animation_walk_left_END = 163
+animation_mine_START = 165
+animation_mine_END = 179
 
 -- Set mesh for all players
 function switch_player_visual()
@@ -28,7 +35,7 @@ function switch_player_visual()
 	
 	for _, obj in pairs(minetest.get_connected_players()) do
 		obj:set_properties(prop)
-		obj:set_animation({x=animation_walk_forward_START, y=animation_walk_forward_END}, 30, 0)
+		obj:set_animation({x=animation_stand_START, y=animation_walk_forward_END}, animation_speed, animation_blend)
 	end
 
 	minetest.after(10.0, switch_player_visual)
